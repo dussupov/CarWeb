9f2edf94d129bdd5d9ab6801f6bc052c7c23aa41
Руслан <rd@ilab.kz>
2023-12-25 11:41:56 +0600

    first commit

---
+++ script/dist/script.dev.js
@@ -0,0 +1,55 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  // При загрузке страницы устанавливаем позицию скролла в верхнюю часть
+  window.scrollTo(0, 0);
+  var lastScrollTop = 0;
+  window.addEventListener("scroll", function () {
+    var welcomeScreen = document.querySelector(".welcome-screen");
+    var blackCircle = document.querySelector(".black-circle");
+    var mainLanding = document.querySelector('.main-landing');
+    var scrollPosition = window.scrollY;
+    var headerLogo = this.document.querySelector('.header-logo');
+
+    if (scrollPosition > lastScrollTop) {
+      // Прокрутка вниз
+      if (scrollPosition < window.innerHeight) {
+        var circleSize = (window.innerHeight - scrollPosition) * 2;
+        blackCircle.style.width = "3000px";
+        blackCircle.style.height = "3000px";
+        blackCircle.style.borderRadius = "0";
+        this.setTimeout(function () {
+          mainLanding.classList.add('show');
+          headerLogo.classList.remove('hd');
+        }, [300]);
+      } else {
+        welcomeScreen.style.display = "none";
+      }
+    }
+
+    lastScrollTop = scrollPosition;
+  }); // Обработчик перед выгрузкой страницы
+
+  window.addEventListener("beforeunload", function () {
+    window.scrollTo(0, 0);
+  });
+  var observer = new IntersectionObserver(function (en) {
+    en.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+  var hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach(function (el) {
+    return observer.observe(el);
+  });
+  var burger = document.querySelector('.header-burger');
+  var burgerContent = document.querySelector('.header-burger__content');
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('show');
+    burgerContent.classList.toggle('show');
+  });
+});
---
+++ script/script.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // При загрузке страницы устанавливаем позицию скролла в верхнюю часть
+    window.scrollTo(0, 0);
+
+    let lastScrollTop = 0;
+
+    window.addEventListener("scroll", function () {
+        const welcomeScreen = document.querySelector(".welcome-screen");
+        const blackCircle = document.querySelector(".black-circle");
+        const mainLanding = document.querySelector('.main-landing')
+        const scrollPosition = window.scrollY;
+        const headerLogo = this.document.querySelector('.header-logo')
+
+        if (scrollPosition > lastScrollTop) {
+            // Прокрутка вниз
+            if (scrollPosition < window.innerHeight) {
+                const circleSize = (window.innerHeight - scrollPosition) * 2;
+                blackCircle.style.width = `3000px`;
+                blackCircle.style.height = `3000px`;
+                blackCircle.style.borderRadius = `0`;
+                this.setTimeout(()=>{
+                    mainLanding.classList.add('show')
+                    headerLogo.classList.remove('hd')
+                },[300])
+            } else {
+                welcomeScreen.style.display = "none";
+            }
+        }
+
+        lastScrollTop = scrollPosition;
+    });
+
+    // Обработчик перед выгрузкой страницы
+    window.addEventListener("beforeunload", function () {
+        window.scrollTo(0, 0);
+    });
+
+    const observer = new IntersectionObserver((en) =>{
+        en.forEach((entry) =>{
+            if(entry.isIntersecting){
+                entry.target.classList.add('show')
+            }else{
+                entry.target.classList.remove('show')
+            }
+        })
+    })
+
+    const hiddenElements = document.querySelectorAll('.hidden')
+    hiddenElements.forEach((el) => observer.observe(el))
+
+    let burger = document.querySelector('.header-burger')
+    let burgerContent = document.querySelector('.header-burger__content')
+
+    burger.addEventListener('click', ()=>{
+        burger.classList.toggle('show')
+        burgerContent.classList.toggle('show')
+    })
+});
